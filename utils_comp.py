8d90cf2bc6c36afdf0ccd16d22dd913644c731fc
Oskar Mikal Andreassen <oskarmikal.andreassen@gmail.com>
2024-04-06 09:09:07 +0200

    init push

--- utils_comp.py
+++ utils_comp.py
@@ -1,9 +1,14 @@
 from matplotlib import pyplot as plt
+from matplotlib.patches import Patch
 import numpy as np
 import sympy as sp
 import pandas as pd
 import ast
 import pickle
+import random
+
+SUP_TITLE_X, SUP_TITLE_HA, SUP_TITLE_FONTSIZE = 0.285, 'center', 16
+SUB_PLOTS_FIGSIZE, SUB_LOC, SUB_TITLE_FONTSIZE = (20, 6), 'left', 14
 
 with open('graph_objects/G_simple_directed.pickle', 'rb') as f:
     G_simple_directed = pickle.load(f)
@@ -13,63 +18,88 @@ NUM_NODES_IN_G_SIMPLE_DIRECTED = G_simple_directed.number_of_nodes()
 NUM_EDGES_IN_G_SIMPLE_DIRECTED = G_simple_directed.number_of_edges()
 
 
-def plot_metrics(df1, df2, index1, index2):
-    min_length = min(len(df1), len(df2))
-    df1 = df1.iloc[:min_length]
-    df2 = df2.iloc[:min_length]
+import numpy as np
+import sympy as sp
+import matplotlib.pyplot as plt
 
-    plt.figure(figsize=(10, 6))
-    plt.plot(df1.index, df1[index1], label=f'{index1}')
-    plt.plot(df2.index, df2[index2], label=f'{index2}')
+def plot_metrics(df1_pair, df2_pair, index1, index2):
+    
+    fig, axs = plt.subplots(1, 2, figsize=SUB_PLOTS_FIGSIZE)
+    entity_type = 'node' if ',' not in str(df2_pair[0].iloc[-1]['removed_entity']) else 'edge'
 
-    # Add vertical line at intersection points
-    intersection_points = np.argwhere(np.diff(np.sign(df1[index1] - df2[index2]))).flatten()
-    for point in intersection_points:
-        if point != 0:
-            plt.axvline(x=point, color='r', linestyle='--', alpha=0.4)
-            plt.text(point + 1, 0.9, 'k=' + str(point), rotation=0, alpha=0.4, color='r', verticalalignment='top')
-  
-    # Calculate absolute difference between the two curves
-    plt.fill_between(df1.index, 0, np.abs(df1[index1] - df2[index2]), color='grey', alpha=0.1)
+
+    for ax, df1, df2 in zip(axs, df1_pair, df2_pair):
+        min_length = min(len(df1), len(df2))
+        df1 = df1.iloc[:min_length]
+        df2 = df2.iloc[:min_length]
+
+        heuristic = 'random' if 'random' in str(df1.iloc[-1]['heuristic']) else 'greedy'
+
+        ax.set_title(f'{heuristic} heuristic', loc=SUB_LOC, fontsize=SUB_TITLE_FONTSIZE)
+        ax.plot(df1.index, df1[index1], label=f'{index1}')
+        ax.plot(df2.index, df2[index2], label=f'{index2}')
+
+        # Add vertical line at intersection points
+        intersection_points = np.argwhere(np.diff(np.sign(df1[index1] - df2[index2]))).flatten()
+        for point in intersection_points:
+            if point != 0:
+                ax.axvline(x=point, color='r', linestyle='--', alpha=0.4)
+                ax.text(point + 2, 0.9, 'k=' + str(point), rotation=0, alpha=0.4, color='r', verticalalignment='top')
+        
+        # Calculate absolute difference between the two curves
+        ax.fill_between(df1.index, 0, np.abs(df1[index1] - df2[index2]), color='grey', alpha=0.1)
+        ax.set_xlabel('k '+entity_type+' removals')
+
+    # Creating a separate legend for the intersection and absolute difference
+    intersection_legend = plt.Line2D([0], [0], color='r', linestyle='--', alpha=0.4, label='intersection')
+    abs_diff_legend = Patch(facecolor='grey', edgecolor='black', alpha=0.1, label='absolute diff')
+    fig.legend(handles=[intersection_legend, abs_diff_legend], loc='upper right', bbox_to_anchor=(0.75, 1.0))
+
+    # Combine handles and labels for plot lines
+    handles, labels = axs[0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='upper right', bbox_to_anchor=(0.9, 1.0))
+
+    plt.suptitle('Comparison of max flow and centrality based N-k anlyses', x=SUP_TITLE_X, ha=SUP_TITLE_HA, fontsize=SUP_TITLE_FONTSIZE)
+    plt.show()
+    
+    for df1, df2 in zip(df1_pair, df2_pair):
+        min_length = min(len(df1), len(df2))
+        df1 = df1.iloc[:min_length]
+        df2 = df2.iloc[:min_length]
+
+        # Calculate and print AUC for each series
+        auc1 = round(np.trapz(df1[index1], df1.index), 2)
+        auc2 = round(np.trapz(df2[index2], df2.index), 2)
+
+        # Fit a polynomial of degree 2 (change this to fit a polynomial of a different degree)
+        coeffs1 = np.polyfit(df1.index, df1[index1], 3)
+        coeffs2 = np.polyfit(df2.index, df2[index2], 3)
+
+        # Calculate curvature for polynomial fits
+        x = sp.Symbol('x')
+        f1 = coeffs1[0]*x**2 + coeffs1[1]*x**2 + coeffs1[2]*x + coeffs1[3]
+        f2 = coeffs2[0]*x**2 + coeffs2[1]*x**2 + coeffs2[2]*x + coeffs2[3]
+
+        f1_prime, f2_prime = sp.diff(f1, x), sp.diff(f2, x)
+        f1_double_prime, f2_double_prime = sp.diff(f1_prime, x), sp.diff(f2_prime, x)
+
+        curvature_formula1 = sp.Abs(f1_double_prime) / (1 + f1_prime**2)**(3/2)
+        curvature_formula2 = sp.Abs(f2_double_prime) / (1 + f2_prime**2)**(3/2)
+
+        interval_start, interval_end = min(df1.index[0], df2.index[0]), max(df1.index[min_length-1], df2.index[min_length-1])
+        interval_points = np.linspace(interval_start, interval_end, 100)
+        curvature_values1, curvature_values2 = [curvature_formula1.subs(x, point) for point in interval_points], [curvature_formula2.subs(x, point) for point in interval_points]
+
+        # Calculate max curvature and average curvature
+        max_curvature1, max_curvature2 = round(max(curvature_values1) * 1000, 5), round(max(curvature_values2) * 1000, 5)
+        average_curvature1,average_curvature2= round(np.mean(curvature_values1) * 1000, 5), round(np.mean(curvature_values2) * 1000, 5)
+
+        # Print AUC and ROC information side by side
+        print(f"\tAUC for {index1}: {auc1}  \t\t\t\t\t\t\t          AUC for {index2}: {auc2}")
+        print(f"\t{index1}: [max ROC: {max_curvature1}, avg ROC: {average_curvature1}]    \t\t\t\t\t  {index2}: [max ROC: {max_curvature2}, avg ROC: {average_curvature2}]\n")
 
     
-    # Calculate and print AUC for each series
-    auc1 = round(np.trapz(df1[index1], df1.index), 2)
-    auc2 = round(np.trapz(df2[index2], df2.index), 2)
-    print(f"AUC for {index1}: {auc1}")
-    print(f"AUC for {index2}: {auc2}", end='\n\n')
 
-    # Fit a polynomial of degree 2 (change this to fit a polynomial of a different degree)
-    coeffs1 = np.polyfit(df1.index, df1[index1], 3)
-    coeffs2 = np.polyfit(df2.index, df2[index2], 3)
-
-    # Calculate curvature for polynomial fits
-    x = sp.Symbol('x')
-    f1 = coeffs1[0]*x**2 + coeffs1[1]*x**2 + coeffs1[2]*x + coeffs1[3]
-    f2 = coeffs2[0]*x**2 + coeffs2[1]*x**2 + coeffs2[2]*x + coeffs2[3]
-
-    f1_prime, f2_prime = sp.diff(f1, x), sp.diff(f2, x)
-    f1_double_prime, f2_double_prime = sp.diff(f1_prime, x), sp.diff(f2_prime, x)
-
-    curvature_formula1 = sp.Abs(f1_double_prime) / (1 + f1_prime**2)**(3/2)
-    curvature_formula2 = sp.Abs(f2_double_prime) / (1 + f2_prime**2)**(3/2)
-
-    interval_start, interval_end = min(df1.index[0], df2.index[0]), max(df1.index[min_length-1], df2.index[min_length-1])
-    interval_points = np.linspace(interval_start, interval_end, 100)
-    curvature_values1, curvature_values2 = [curvature_formula1.subs(x, point) for point in interval_points], [curvature_formula2.subs(x, point) for point in interval_points]
-
-    # Calculate max curvature and average curvature
-    max_curvature1, max_curvature2 = round(max(curvature_values1) * 1000, 5), round(max(curvature_values2) * 1000, 5)
-    average_curvature1,average_curvature2= round(np.mean(curvature_values1) * 1000, 5), round(np.mean(curvature_values2) * 1000, 5)
-
-    print(f"{index1}: [max ROC: {max_curvature1}, avg ROC: {average_curvature1}]")
-    print(f"{index2}: [max ROC: {max_curvature2}, avg ROC: {average_curvature2}]", end='\n\n')
-
-    plt.xlabel('k iterations')
-    plt.ylabel('index')
-    plt.title('Network performance index versus k entity removals')
-    plt.legend()
-    plt.show()
 
 
 def common_entities(df1_, df2_):
@@ -126,14 +156,48 @@ def common_entities(df1_, df2_):
         result_df['removed_entity'] = result_df['removed_entity'].apply(lambda x: tuple(set(ast.literal_eval(x))))
     print(f"{len(result_df)} common entity removals")
     print(f"{len(df2)} greedy entity removals before '{df2.iloc[0]['heuristic']}' reached 0 (limiting metric)\n")
-    
 
+    def calculate_variance(entities, num_entities_df1, num_entities_df2):
+        """
+        Calculate the variance of the differences in index positions of unique numbers in two lists of entities. 
+        """
+        total_variance = 0
+        num_iterations = 100
+        for _ in range(num_iterations):
+            # Define a sequence of unique numbers in the range of entities
+            unique_numbers = list(range(entities))
+
+            # Generate two lists, one for df1 and one for df2
+            df1_list = random.sample(unique_numbers, min(entities, num_entities_df1))
+            df2_list = random.sample(unique_numbers, min(entities, num_entities_df2))
+
+            # Calculate differences in index positions of unique numbers
+            differences = []
+            for num in unique_numbers:
+                if num in df1_list and num in df2_list:
+                    diff = abs(df1_list.index(num) - df2_list.index(num))
+                    differences.append(diff)
+
+            # Calculate variance
+            if differences:  # Check if differences list is not empty
+                variance = sum((x - (sum(differences) / len(differences))) ** 2 for x in differences) / len(differences)
+                total_variance += variance
+
+        average_variance = total_variance / num_iterations if total_variance > 0 else 0  # Handle division by zero
+        return round(average_variance, 0)
 
     # Calculate and print the variance of the diffs list
     print(f"Variance of index differences of common entities: {round(np.var(index_diffs),0)}")
+    if '[' in df1.iloc[0]['removed_entity']:
+        print(f"Variance of index differences for random (averaged) edge removal: {calculate_variance(NUM_EDGES_IN_G_SIMPLE_DIRECTED, len(df1), len(df2))}")
+    else:
+        print(f"Variance of index differences for random (averaged) node removal: {calculate_variance(NUM_NODES_IN_G_SIMPLE_DIRECTED, len(df1), len(df2))}")
+
+    # Take the absolute value of the 'diff' column
+    result_df['abs_diff'] = result_df['diff'].abs()
+    result_df.drop(columns='diff', inplace=True)
+
+    # Sort the DataFrame by 'diff' in ascending order
+    result_df = result_df.sort_values(by='abs_diff')
 
     return result_df
-
-
-  
-
